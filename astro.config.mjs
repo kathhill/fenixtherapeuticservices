1cc54d96bc3830a783979eee968c8f051b519d9a
kathhill <kath.hill@sbcglobal.net>
2024-09-18 10:20:25 -0700

    feat: Add xml sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [partytown()],
-});
+  site: 'https://fenixtherapeuticservices.com',
+  integrations: [
+    partytown(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.85,
+      lastmod: new Date(),
+    }),
+  ],
+}); 
